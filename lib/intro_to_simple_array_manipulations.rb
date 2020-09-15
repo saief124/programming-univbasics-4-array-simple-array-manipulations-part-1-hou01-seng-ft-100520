78c777405384fb50268b92731cbe6af2eb24cf88
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-15 00:24:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,23 +1,23 @@
-def using_push{array, string}
+def using_push(array, string)
 array.push(string)
 end
 
-def using_unshift{array, string}
+def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_pop{array}
+def using_pop(array)
   array.pop
 end
 
-def pop_with_args{array}
+def pop_with_args(array)
 array.pop(2)
 end
 
-def using_shift{array}
+def using_shift(array)
   array.shift
 end
 
-def shift_with_args{array}
+def shift_with_args(array)
   array.shift(2)
 end
